3f2993fc4105bd27c97910de85f8acc24d41d866
katie zhu <to.kzhu@gmail.com>
2016-10-05 23:44:38 -0700

    fixes for ios selection, hopefully gets rid of white strip

--- dist/toolkit.js
+++ dist/toolkit.js
@@ -2339,6 +2339,7 @@ var videoPlayer = document.getElementsByClassName('video-content')[0]
 var rsvpLink = document.getElementsByClassName('js-rsvpLink')[0]
 var notification = document.getElementsByClassName('js-notification')[0]
 var rsvpContainer = document.getElementsByClassName('js-rsvpContainer')[0]
+var isMobile = navigator.userAgent.indexOf('Mobile') > 0
 
 videoPlayer.addEventListener('ended', function () {
   this.pause()
@@ -2368,32 +2369,41 @@ videoPlayer.addEventListener('ended', function () {
 videoPlayer.play()
 
 rsvpLink.addEventListener('click', function (e) {
-  e.preventDefault()
+  var email = document.getElementsByClassName('js-rsvpEmail')[0]
 
-  var email = 'rsvp@matter.com'
-  var textArea = document.createElement('textarea')
+  if (!isMobile) {
+    var emailText = 'rsvp@matter.com'
+    var textArea = document.createElement('textarea')
+    textArea.style.position = 'fixed'
+    textArea.style.top = 0
+    textArea.style.left = 0
+    textArea.style.width = '2em'
+    textArea.style.height = '2em'
+    textArea.style.padding = 0
+    textArea.style.border = 'none'
+    textArea.style.outline = 'none'
+    textArea.style.boxShadow = 'none'
+    textArea.style.background = 'transparent'
 
-  textArea.style.position = 'fixed'
-  textArea.style.top = 0
-  textArea.style.left = 0
-  textArea.style.width = '2em'
-  textArea.style.height = '2em'
-  textArea.style.padding = 0
-  textArea.style.border = 'none'
-  textArea.style.outline = 'none'
-  textArea.style.boxShadow = 'none'
-  textArea.style.background = 'transparent'
+    textArea.value = emailText
+    document.body.appendChild(textArea)
 
-  textArea.value = email
-  document.body.appendChild(textArea)
-
-  textArea.select()
-  document.execCommand('copy')
-  document.body.removeChild(textArea)
+    textArea.select()
+    document.execCommand('copy', false, null)
+    document.body.removeChild(textArea)
+  } else {
+    var selection = window.getSelection()
+    var range = document.createRange()
+    range.selectNodeContents(email)
+    selection.removeAllRanges()
+    selection.addRange(range)
+    $(email).select()
+  }
 
   rsvpContainer.classList.add('is-selected')
-  rsvpLink.innerHTML = 'rsvp@matter.com'
   notification.classList.remove('hidden')
+  email.classList.remove('hidden')
+  rsvpLink.classList.add('hidden')
 })
 
 +function ($) {
