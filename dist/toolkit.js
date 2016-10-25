ac0040478d892a1291b10b25f521ad4a3ce606ec
katie zhu <to.kzhu@gmail.com>
2016-10-25 13:38:20 -0700

    add getalong quiz redirect landing page

--- dist/toolkit.js
+++ dist/toolkit.js
@@ -2335,61 +2335,85 @@
 
 }(jQuery);
 
+// FAQ PAGE
 var questions = document.getElementsByClassName('js-question')
-var initialFontSize = getComputedStyle(questions[0]).getPropertyValue('font-size')
-var initialFontSizeNumber = Number(initialFontSize.split('px')[0])
 
-var previousFontSizeNumber
+if (questions.length) {
+  var initialFontSize = getComputedStyle(questions[0]).getPropertyValue('font-size')
+  var initialFontSizeNumber = Number(initialFontSize.split('px')[0])
+  var previousFontSizeNumber
 
-for (var i = 0; i < questions.length; i++) {
-  var q = questions[i]
-  q.addEventListener('click', toggleAnswer)
-}
+  for (var i = 0; i < questions.length; i++) {
+    var q = questions[i]
+    q.addEventListener('click', toggleAnswer)
+  }
 
-function toggleAnswer() {
-  var questionNumber = this.getAttribute('data-question-number')
-  var itemEl = this.parentElement.classList.add('is-expanded')
-  var answerEl = document.getElementById('a' + questionNumber)
-  var svgDiv = document.getElementsByClassName('js-effect' + questionNumber)[0]
+  function toggleAnswer() {
+    var questionNumber = this.getAttribute('data-question-number')
+    var itemEl = this.parentElement.classList.add('is-expanded')
+    var answerEl = document.getElementById('a' + questionNumber)
+    var svgDiv = document.getElementsByClassName('js-effect' + questionNumber)[0]
 
-  if (answerEl.className.indexOf('collapse in') < 0) {
-    addEffect(svgDiv, questionNumber)
-  } else {
-    removeEffect(svgDiv, questionNumber)
+    if (answerEl.className.indexOf('collapse in') < 0) {
+      addEffect(svgDiv, questionNumber)
+    } else {
+      removeEffect(svgDiv, questionNumber)
+    }
+  }
+
+  function addEffect(svgDiv, questionNumber) {
+    var svgList = [
+      'poof',
+      'pop',
+      'sparkles',
+      'swoosh1',
+      'swoosh2',
+      'swoosh3',
+      'swoosh4',
+      'swoosh5'
+    ]
+
+    var svgId = svgList[Math.floor(Math.random() * svgList.length)]
+    svgDiv.firstElementChild.setAttribute('src', 'assets/img/' + svgId + '.svg')
+    svgDiv.classList.remove('hidden')
+  }
+
+  function removeEffect(svgDiv, questionNumber) {
+    var openEffectDiv = svgDiv.firstElementChild
+    var closeEffectDiv = svgDiv.querySelector('.effect-img--slam')
+
+    openEffectDiv.classList.add('hidden')
+    closeEffectDiv.classList.remove('hidden')
+
+    closeEffectDiv.addEventListener('animationend', function () {
+      svgDiv.classList.add('hidden')
+      openEffectDiv.classList.remove('hidden')
+      closeEffectDiv.classList.add('hidden')
+      svgDiv.firstElementChild.removeAttribute('src')
+    }, false)
   }
 }
 
-function addEffect(svgDiv, questionNumber) {
-  var svgList = [
-    'poof',
-    'pop',
-    'sparkles',
-    'swoosh1',
-    'swoosh2',
-    'swoosh3',
-    'swoosh4',
-    'swoosh5'
-  ]
 
-  var svgId = svgList[Math.floor(Math.random() * svgList.length)]
-  svgDiv.firstElementChild.setAttribute('src', 'assets/img/' + svgId + '.svg')
-  svgDiv.classList.remove('hidden')
-}
+// GET ALONG GIF PAGE
 
-function removeEffect(svgDiv, questionNumber) {
-  var openEffectDiv = svgDiv.firstElementChild
-  var closeEffectDiv = svgDiv.querySelector('.effect-img--slam')
+var gifList = [
+  'getalong1',
+  'getalong2',
+  'getalong3',
+  'getalong4',
+  'getalong5',
+  'getalong6',
+  'getalong7',
+  'getalong8',
+  'getalong9',
+  'getalong10'
+]
 
-  openEffectDiv.classList.add('hidden')
-  closeEffectDiv.classList.remove('hidden')
+var gifId = gifList[Math.floor(Math.random() * gifList.length)]
+var gifEl = document.getElementsByClassName('js-gif')[0]
 
-  closeEffectDiv.addEventListener('animationend', function () {
-    svgDiv.classList.add('hidden')
-    openEffectDiv.classList.remove('hidden')
-    closeEffectDiv.classList.add('hidden')
-    svgDiv.firstElementChild.removeAttribute('src')
-  }, false)
-}
+if (gifEl) gifEl.setAttribute('src', 'assets/img/' + gifId + '.gif')
 
 +function ($) {
   'use strict';
