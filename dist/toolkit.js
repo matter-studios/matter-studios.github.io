dbc13f4cf0bb678b854d0a5d362096e8b04b2c25
katie zhu <to.kzhu@gmail.com>
2016-10-25 17:43:33 -0700

    fix animations not working tweak song behavior

--- dist/toolkit.js
+++ dist/toolkit.js
@@ -2337,6 +2337,15 @@
 
 // FAQ PAGE
 var questions = document.getElementsByClassName('js-question')
+var answers = document.getElementsByClassName('js-answer')
+var audio = new Audio('./assets/audio/matter-studios.wav')
+var starterPack = document.getElementsByClassName('image--starterpack')[0]
+
+if (starterPack) {
+  starterPack.addEventListener('click', function () {
+    audio.play()
+  })
+}
 
 if (questions.length) {
   var initialFontSize = getComputedStyle(questions[0]).getPropertyValue('font-size')
@@ -2350,45 +2359,50 @@ if (questions.length) {
     } else if (i % 5 == 0) {
       q.addEventListener('click', toggleAnswer)
     }
+
+    $(answers[i]).on('hide.bs.collapse', function () {
+      audio.pause()
+    })
   }
+}
 
-  function toggleAnswer() {
-    var questionNumber = this.getAttribute('data-question-number')
-    var itemEl = this.parentElement.classList.add('is-expanded')
-    var answerEl = document.getElementById('a' + questionNumber)
-    var svgDiv = document.getElementsByClassName('js-effect' + questionNumber)[0]
+function toggleAnswer() {
+  if (this.className.indexOf('js-question') == -1) return
 
-    if (answerEl.className.indexOf('collapse in') < 0) {
-      addEffect(svgDiv, questionNumber)
-    } else {
-      removeEffect(svgDiv, questionNumber)
-    }
+  var questionNumber = this.getAttribute('data-question-number')
+  var answerEl = document.getElementById('a' + questionNumber)
+  var svgDiv = document.getElementsByClassName('js-effect' + questionNumber)[0]
+
+  if (answerEl.className.indexOf('collapse in') < 0) {
+    addEffect(svgDiv, questionNumber)
+  } else {
+    removeEffect(svgDiv, questionNumber)
   }
+}
 
-  function addEffect(svgDiv, questionNumber) {
-    var svgList = [
-      'swoosh4'
-    ]
+function addEffect(svgDiv, questionNumber) {
+  var svgList = [
+    'swoosh4'
+  ]
 
-    var svgId = svgList[Math.floor(Math.random() * svgList.length)]
-    svgDiv.firstElementChild.setAttribute('src', 'assets/img/' + svgId + '.svg')
-    svgDiv.classList.remove('hidden')
-  }
+  var svgId = svgList[Math.floor(Math.random() * svgList.length)]
+  svgDiv.firstElementChild.setAttribute('src', 'assets/img/' + svgId + '.svg')
+  svgDiv.classList.remove('hidden')
+}
 
-  function removeEffect(svgDiv, questionNumber) {
-    var openEffectDiv = svgDiv.firstElementChild
-    var closeEffectDiv = svgDiv.querySelector('.effect-img--slam')
+function removeEffect(svgDiv, questionNumber) {
+  var openEffectDiv = svgDiv.firstElementChild
+  var closeEffectDiv = svgDiv.querySelector('.effect-img--slam')
 
-    openEffectDiv.classList.add('hidden')
-    closeEffectDiv.classList.remove('hidden')
+  openEffectDiv.classList.add('hidden')
+  closeEffectDiv.classList.remove('hidden')
 
-    closeEffectDiv.addEventListener('animationend', function () {
-      svgDiv.classList.add('hidden')
-      openEffectDiv.classList.remove('hidden')
-      closeEffectDiv.classList.add('hidden')
-      svgDiv.firstElementChild.removeAttribute('src')
-    }, false)
-  }
+  closeEffectDiv.addEventListener('animationend', function () {
+    svgDiv.classList.add('hidden')
+    openEffectDiv.classList.remove('hidden')
+    closeEffectDiv.classList.add('hidden')
+    openEffectDiv.removeAttribute('src')
+  }, false)
 }
 
 
